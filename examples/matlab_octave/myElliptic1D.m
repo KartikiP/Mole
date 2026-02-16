b5512458bfd270659ea365a337fe095c6d0dd98f
Kartiki Pande <kpande3289@sdsu.edu>
2026-02-16 11:09:55 -0800

    Update boundary conditions and operator parameters
    My Elliptic1D file
    
    Signed-off-by: Kartiki Pande <kpande3289@sdsu.edu>

--- examples/matlab_octave/myElliptic1D.m
+++ examples/matlab_octave/myElliptic1D.m
@@ -10,18 +10,19 @@ addpath('../../src/matlab_octave')
 west = 0;  % Domain's limits
 east = 1;
 
-k = 6;  % Operator's order of accuracy
-m = 2*k+1;  % Minimum number of cells to attain the desired accuracy
+k = 4;  % Operator's order of accuracy
+m = 10;  % Minimum number of cells to attain the desired accuracy
 dx = (east-west)/m;  % Step length
 grid = [west west+dx/2 : dx : east-dx/2 east]; % grid
 
 % Robin boundary conditions
-dc = [1;1];
-nc = [1;1];
-v = [0;2*exp(1)];
+dc = [0; 1];   % Dirichlet coefficients
+nc = [1; 0];   % Neumann coefficients
+v  = [2; 1];   % RHS values for the BC
+
 
 L = lap(k, m, dx);  % 1D Mimetic laplacian operator
-U = exp(grid)'; % RHS
+U = ones(size(grid))'; % RHS
 [L0,U0] = addScalarBC1D(L,U,k,m,dx,dc,nc,v); % add BC to system
 U0 = L0\U0;  % Solve a linear system of equations
 
